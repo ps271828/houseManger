38adbdeb03eac5b6ace2581f78996aa91cb02178
jht108 <2633790462@qq.com>
2019-06-13 23:17:04 +0800

    重写toString方法

--- src/main/java/com/houses/common/model/ItemCrack.java
+++ src/main/java/com/houses/common/model/ItemCrack.java
@@ -97,4 +97,30 @@ public class ItemCrack extends BaseDao {
     public void setExampleImage(String exampleImage) {
         this.exampleImage = exampleImage;
     }
+    @Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		if("1".equals(crackType)) {
+//			 墙面 1--空鼓 2--龟裂 */
+			sb.append("墙面出现空鼓，");
+		}else if("2".equals(crackType)) {
+			sb.append("墙面出现龟裂，");
+		}
+		switch(wallDamage) {
+			case 1:
+			sb.append("裂缝方向为斜方向，");break;
+			case 2:
+				sb.append("裂缝方向为竖直方向，");break;
+			case 3:
+				sb.append("裂缝方向为水平方向，");break;
+			case 4:
+				sb.append("裂缝方向为不规则方向，");break;
+			default:
+				break;
+		}
+		sb.append("最大长度为" + maxLength + "毫米，");
+		sb.append("最大宽度为" + maxWidth + "毫米，");
+		sb.append("如图");
+		return sb.toString();
+	}
 }
